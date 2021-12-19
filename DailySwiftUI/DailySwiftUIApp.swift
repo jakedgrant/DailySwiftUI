d3c37537f4266b94b369685dc9ba23b89522c665
jakedgrant <jake.d.grant@gmail.com>
2021-12-19 14:14:45 -0600

    Initial Commit

---
+++ DailySwiftUI/DailySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailySwiftUIApp.swift
+//  DailySwiftUI
+//
+//  Created by Jake Grant on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DailySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
